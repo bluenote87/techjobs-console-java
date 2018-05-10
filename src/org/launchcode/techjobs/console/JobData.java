7cf202cb5275d8dba2e6b5873e86e37d3327d4e8
Paul Fannon <lurchworld@gmail.com>
2018-05-10 11:09:20 -0500

    Made a first draft of findByValue method

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -79,6 +80,31 @@ public class JobData {
             if (aValue.contains(value)) {
                 jobs.add(row);
             }
+        }
+
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+                HashMap<String, String> hashJob = new HashMap<>();
+                hashJob = row;
+
+                    for (Map.Entry<String, String> data : hashJob.entrySet()) {
+
+                        String aValue = data.getValue();
+
+                        if (aValue.contains(value)) {
+                            jobs.add(row);
+                            break;
+                        }
+                    }
         }
 
         return jobs;
